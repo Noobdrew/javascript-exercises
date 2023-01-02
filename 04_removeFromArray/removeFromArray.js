a9eaf23dc25675909ed9391c49533ed4828f26b6
Noobdrew <noobdrew23@gmail.com>
2023-01-02 20:40:03 +0200

    rework it to use array.filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+let i =1
 
+console.log(arguments.indexOf(array[i]))
+
+    return array
 };
 
+removeFromArray([1, 2, 3, 4], 2)
 // Do not edit below this line
 module.exports = removeFromArray;
