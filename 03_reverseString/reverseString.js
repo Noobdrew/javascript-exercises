ebd2ebe61aaf7a794f8d72c52c52648b94adeb40
Noobdrew <noobdrew23@gmail.com>
2023-01-02 17:12:01 +0200

    reverse string complete
    string reversed with by converting str to array reversing array and converting array to string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let arrayString = string.split('')
+    arrayString.reverse();
+    console.log(arrayString)
+    let revString = arrayString.join("")
+    console.log(revString)
+    return revString
 };
-
+reverseString('hello')
 // Do not edit below this line
 module.exports = reverseString;
