29266b818c26dd3bbd701e31d71c30e5dbccd873
Noobdrew <noobdrew23@gmail.com>
2023-01-02 17:02:59 +0200

    first test complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+let word=string
+
+for (let i =1;i<num;i++){
+    
+    word += string
+}
+if(num===0){
+word = ""
+} else if (num<0){
+word = "ERROR"
+}
+console.log(word)
+return word
 };
+
+repeatString('hey', -5)
+
 
 // Do not edit below this line
 module.exports = repeatString;
