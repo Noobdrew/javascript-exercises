2ff43279fcc0509c96d82d1a53c0677365d7fd56
Noobdrew <noobdrew23@gmail.com>
2023-01-03 16:49:31 +0200

    remove from array complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -20,27 +20,7 @@ const removeFromArray = function () {
 
 
 removeFromArray([1, 2, 3, 4], 3)
-//7 is false move to next value
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
-
-
-
-
-// const array = [1, 2, 3, 4, 5];
-
-// function isPrime(num) {
-//     for (let i = 2; num > i; i++) {
-//         console.log(`this is num ${num} in cycle i ${i}`)
-//         if (num % i === 0) {
-//             console.log(`this is num ${num} in cycle i ${i} when it returns false`)
-//             return false;
-//         }
-//     }
-//     // goes to here only when num>i is not true so it goes for all i numbers up till then
-//     console.log(`this is num ${num} when it returns true`)
-//     return true
-// }
-
-// console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
