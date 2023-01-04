9b3920b37d8889d82913711fa4fc4d69b7c11ec0
Noobdrew <noobdrew23@gmail.com>
2023-01-04 16:08:30 +0200

    calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,47 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  console.log(num1 + num2)
+  return (num1 + num2)
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  console.log(num1 - num2)
+  return (num1 - num2)
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  const sum = array.reduce((partialSum, a) => partialSum + a, 0);
+  console.log(sum);
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  const sum = array.reduce((partialSum, a) => partialSum * a, 1);
+  console.log(sum);
+  return sum
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  console.log(num1 ** num2)
+  return (num1 ** num2)
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let sum =1
+  while (num > 0) {
+    sum *=num
+    num--
+  }
+  console.log(sum)
+  return sum
 };
+
+
+// add(0, 0);
+// subtract(10, 4)
+// sum([7, 11])
+// multiply([2, 3])
+// power(2, 3)
+factorial(0)
 
 // Do not edit below this line
 module.exports = {
