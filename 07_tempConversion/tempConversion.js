c239e5d630f805930012b6f35aad81ba1fa4e315
Noobdrew <noobdrew23@gmail.com>
2023-01-04 15:48:14 +0200

    temp conversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // f=(x-32)*5/9
+  let roundedFarenheit = Math.round((temp - 32) * (5 / 9) * 10) / 10
+
+  console.log(roundedFarenheit)
+  return roundedFarenheit
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  //c=(x*9/5+32)
+  let roundedCelsius = Math.round((temp * (9 / 5) + 32) * 10) / 10
+
+  console.log(roundedCelsius)
+  return roundedCelsius
 };
+
+convertToCelsius(100)
+convertToFahrenheit(0)
 
 // Do not edit below this line
 module.exports = {
