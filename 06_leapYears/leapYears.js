83ed19c721da6052f347ad93285c0feefb412d61
Noobdrew <noobdrew23@gmail.com>
2023-01-04 15:33:21 +0200

    leap years complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function (year) {
+
+    //%4==0 for first check (they are leap years)
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        console.log('Leap year first check')
+        return true
+    } else {
+        console.log('not leap year')
+        return false
+    }
+    //%100==0 for second check (They are not leap years)
+    //%400==0 only if it is 100%==0
 
 };
 
+leapYears(1600)
 // Do not edit below this line
 module.exports = leapYears;
