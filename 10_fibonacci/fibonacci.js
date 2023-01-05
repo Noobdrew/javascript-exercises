b82306e80193c36375945909be21724aba7d5827
Noobdrew <noobdrew23@gmail.com>
2023-01-05 15:57:00 +0200

    i had to google it
    why woudnt i google if i can

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    //function convers str to int but does not accept negative numbers
+    let numInt = parseInt(num)
+    if (numInt < 0) {
+        return "OOPS"
+    }
 
+    let a = 0, b = 1, c = numInt;
+  
+    for(let i = 2; i <= numInt; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    console.log(c)
+    return c
 };
 
+fibonacci(5)
 // Do not edit below this line
 module.exports = fibonacci;
