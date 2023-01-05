0d5ed684b593e2f856ba837342c8a5e26c97df58
Noobdrew <noobdrew23@gmail.com>
2023-01-05 16:26:46 +0200

    complete by using array.map method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]
+console.log(books)
 
+const getTheTitles = function () {
+
+    //go through books array and extract elements by object property title
+    let result = books.map(a => a.title);
+    console.log(result)
+    return result
 };
 
+
+getTheTitles(books)
 // Do not edit below this line
 module.exports = getTheTitles;
