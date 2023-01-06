ae39ef2e1a9b702b687bc8ab8145f67bdd1cc1ce
Noobdrew <noobdrew23@gmail.com>
2023-01-06 18:16:00 +0200

    fixed comments

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,32 +1,33 @@
 const people = [
     {
-      name: "Carly",
-      yearOfBirth: 1066,
+        name: "Carly",
+        yearOfBirth: 1066,
     },
     {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
     },
     {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
     },
-  ]
+]
 
 const findTheOldest = function () {
+    //check if objects have yearOfDeath and adds current year if they dont
     for (let i = 0; i < people.length; i++) {
         if (!people[i].hasOwnProperty('yearOfDeath')) {
             console.log(`no death on element ${i}`)
             people[i].yearOfDeath = new Date().getFullYear()
         }
     }
-      //use foreach to run people[0].age = people[0].yearOfDeath - people[0].yearOfBirth to add age property
+    //use foreach  to add age property to all objects 
     people.forEach(element => element.age = element.yearOfDeath - element.yearOfBirth)
 
     //compare all the array.age values to find the biggest
-    let oldest = Math.max(...people.map(o => o.age))      //=49
+    let oldest = Math.max(...people.map(o => o.age))      
     //return the argument with oldest age
     for (let i = 0; i < people.length; i++) {
         if (people[i].age == oldest) {
