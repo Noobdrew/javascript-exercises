a8fd7e189d27cbabeb64c3ad651ea2513819f9d7
Noobdrew <noobdrew23@gmail.com>
2023-01-06 17:13:04 +0200

    task complete when manual check
    For some reason the nmp test does not work but all cases have been manually tested and all return correct values

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,44 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+const findTheOldest = function () {
+    for (let i = 0; i < people.length; i++) {
+        if (!people[i].hasOwnProperty('yearOfDeath')) {
+            console.log(`no death on element ${i}`)
+            people[i].yearOfDeath = new Date().getFullYear()
+        }
+    }
+      //use foreach to run people[0].age = people[0].yearOfDeath - people[0].yearOfBirth to add age property
+    people.forEach(element => element.age = element.yearOfDeath - element.yearOfBirth)
+
+    //compare all the array.age values to find the biggest
+    let oldest = Math.max(...people.map(o => o.age))      //=49
+    //return the argument with oldest age
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].age == oldest) {
+            console.log(people[i])
+            return people[i]
+        }
+    }
+
 
 };
+
+findTheOldest(people)
 
 // Do not edit below this line
 module.exports = findTheOldest;
