127286a06f4d9a1fff38c7ddcd7b62e82dccb8cc
Noobdrew <noobdrew23@gmail.com>
2023-01-09 17:16:27 +0200

    works in all cases
    I need to learn more about % operator, and stop using arrays all the time.

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,27 +1,33 @@
 const caesar = function (text, shift) {
-    let array = text.toLowerCase().split('')
-    console.log(array)
-    let oldUni = array.map(x => x.charCodeAt())
-    console.log(oldUni)
-    let newUni = oldUni.map(x => x + shift)
-    console.log(newUni)
-    let newText = newUni.map(
-        function (x) {
-            if (x > 122) {      // || checks for 1st true && checks for 1st false if runs only on true
-                return x = x - 26
-            } else if (x < 97) {
-                return x = x + 26
+    if (shift < 0) {
+        return caesar(text, shift + 26);
+    }
+    let output = ''
+    // goes through all characters 
+    for (let i = 0; i < text.length; i++) {
+        let c = text[i]
+
+        //check if character is letter
+        if (c.match(/[a-z]/i)) {
+            let code = text.charCodeAt(i)
+
+            if (code >= 65 && code <= 90) {
+                //%26 prevents c from being bigger than 90 or 122 so it stays in a-z range 
+                c = String.fromCharCode(((code - 65 + shift) % 26) + 65)
+                
             }
-            else {
-                return x
+            else if (code >= 97 && code <= 122) {
+                c = String.fromCharCode(((code - 97 + shift) % 26) + 97)
             }
-        })
-        .map(x => String.fromCharCode(x)).join('')
-    console.log(newText)
+        }
+        output += c
+    }
+    console.log(output)
+    return output
 };
 
 
 
-caesar('url', -13)
+caesar('Mjqqt, Btwqi!', -5)
 // Do not edit below this line
 module.exports = caesar;
